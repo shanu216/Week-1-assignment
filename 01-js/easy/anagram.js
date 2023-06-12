eb0ab58003a73cb730f81be548e20491320384d7
Shanu <shanu.jha216@gmail.com>
2023-06-12 23:57:04 +0530

    anagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,32 @@
 */
 
 function isAnagram(str1, str2) {
+  let obj1 = {};
+  let obj2 = {};
+  let count = 0;
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  else {
+    for (let i = 0; i < str1.length; i++) {
+      obj1[str1[i]] = obj1[str1[i]] ? obj1[str1[i]] + 1 : 1;
+      obj2[str2[i]] = obj2[str2[i]] ? obj2[str2[i]] + 1 : 1;
+    }
 
+    let keys = Object.keys(obj1);
+
+    for (let key of keys) {
+      if (obj1[key] === obj2[key]) {
+        count = count + 1;
+      }
+    }
+  }
+  if (count == str1.length) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
